9861860bc2bcd9894eec43fc4e4a30bbc9cbfdc1
Piero Maddaleni <piero@piemadd.com>
2020-03-24 16:13:59 +0000

    Final Release lol

---
+++ main.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, send_file, redirect
+from random import randint, uniform
+import numpy as np
+import requests
+from json2html import *
+app = Flask('app')
+
+vids = np.load('vids.npy').tolist()
+
+@app.route('/')
+def index():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return render_template('index/index.html')
+
+@app.route('/landscape')
+def landscape():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return compileLandscape()
+	
+@app.route('/img/<string:id>')
+def returnIMG(id):
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	filename = 'images/' + id + '.jpg'
+	return send_file(filename, mimetype='image/gif')
+
+@app.route('/video')
+def returnvideo():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	global vids
+	id = getrandomvideo(vids)
+	return redirect("https://www.youtube.com/watch?v=" + id, code=302)
+
+@app.route('/favicon.ico')
+def favicon():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return send_file('favicon.ico', mimetype='image/gif')
+
+@app.route('/info')
+def info():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return render_template('info.html')
+
+@app.route('/info/us')
+def info_us():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return get_us()
+
+@app.route('/info/world')
+def info_world():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return get_world()
+
+@app.route('/animal')
+def animal():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return render_template('animal.html')
+
+@app.route('/robots.txt')
+def robots():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	return "User-agent: * Disallow: "
+
+@app.route('/place')
+def place():
+	requests.get('https://www.simple-counter.com/hit.php?id=zroxkx&nd=8&nc=4&bc=1')
+	randx = uniform(-90, 90)
+	randy = uniform(-180, 180)
+	return redirect("https://www.google.com/maps/@" + str(randx) + ',' + str(randy) + ',14z', code=302)
+
+def getrandomvideo(vids):
+	return vids[randint(0, 6899)]
+
+def compileLandscape():
+	filea = open('unfinished/landscape/1.uhtml', 'r')
+	a = filea.read()
+	fileb = open('unfinished/landscape/2.uhtml', 'r')
+	b = fileb.read()
+	rand = randint(1, 17)
+	if len(str(rand)) < 2:
+		rand = '0' + str(rand)
+	else: 
+		rand = str(rand)
+	return a + rand + b
+
+def get_us():
+	yeet = requests.get("https://corona.lmao.ninja/states").content.decode("utf-8")
+	ok = json2html.convert(json = yeet)
+	ok = ok.replace("state", "State")
+	ok = ok.replace("cases", "Cases")
+	ok = ok.replace("todayCases", "Cases Today")
+	ok = ok.replace("deaths", "Deaths")
+	ok = ok.replace("todayDeaths", "Deaths Today")
+	ok = ok.replace("recovered", "Recovered")
+	ok = ok.replace("active", "Active Cases")
+	return '<html><head><title>US Statistics</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css"></head><body><center>' + ok + '</center></body></html>'
+
+def get_world():
+	yeet = requests.get("https://corona.lmao.ninja/jhucsse").content.decode("utf-8")
+	ok = json2html.convert(json = yeet)
+	ok = ok.replace("country", "Country")
+	ok = ok.replace("province", "Most Affected Province")
+	ok = ok.replace("updatedAt", "Last Time Updated")
+	ok = ok.replace("stats", "Statistics")
+	ok = ok.replace("confirmed", "Confirmed Cases")
+	ok = ok.replace("deaths", "Deaths")
+	ok = ok.replace("recovered", "Recovered Cases")
+	ok = ok.replace("coordinates", "Coordinates of Country")
+	ok = ok.replace("latitude", "Latitude")
+	ok = ok.replace("longitude", "Longitude")
+	return '<html><head><title>World Statistics</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css"></head><body><center>' + ok + '</center></body></html>'
+
+app.run(host='0.0.0.0')
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Hello! This is my entry for the Repl.it game Jam from March 16th until March 23rd!
+
+You can check about.txt for some info on whatever I have made or changelog.txt to see all of the changes which I have made!
